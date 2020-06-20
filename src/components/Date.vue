210c1c13219eefc296aa1440af5a1aec654fffdf
Tereza E <t.e.hronova@gmail.com>
2020-06-20 10:00:10 +0200

    Added component "date"

--- src/components/Date.vue
+++ src/components/Date.vue
@@ -1,12 +1,46 @@
 <template>
-  <p>Hello, I am date!</p>
+  <div>
+    <div v-if="isEditable" class="form-item">
+      <input type="date" class="date" v-model="date" />
+      <span class="highlight"></span>
+      <span class="bar date-bar"></span>
+      <label>{{ label }}</label>
+    </div>
+    <p v-else>{{ value }}</p>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "Date",
-  data() {
-    return {};
+  name: 'Date',
+  props: {
+    value: {
+      type: String,
+      required: true
+    },
+    label: {
+      type: String,
+      required: true
+    },
+    isEditable: {
+      type: Boolean,
+      default: true
+    }
+  },
+  watch: {
+    value () {
+      this.$emit('input', this.value)
+      console.log(this.value)
+    }
+  },
+  data () {
+    return {}
   }
-};
+}
 </script>
+
+<style>
+.date {
+  max-width: 200px;
+}
+</style>
