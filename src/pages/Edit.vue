1a87cece231eebbcb9f3be78100c13d56e6c580b
Hana Halamova <halamovahana1@gmail.com>
2020-06-20 13:24:27 +0200

    Fix of missing files

---
+++ src/pages/Edit.vue
@@ -0,0 +1,17 @@
+<template>
+  <MyTemplate :isEditable="true" />
+</template>
+
+<script>
+import FirstTemplate from "../templates/FirstTemplate";
+
+export default {
+  name: "Edit",
+  data() {
+    return {};
+  },
+  components: {
+    MyTemplate: FirstTemplate
+  }
+};
+</script>
