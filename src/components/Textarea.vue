a4cd3e877bdaf25a3bc9bd8a728bc39f24dfa134
Tereza E <t.e.hronova@gmail.com>
2020-06-20 10:35:41 +0200

    Created new component "Textarea"

---
+++ src/components/Textarea.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <div v-if="isEditable" class="form-item">
+      <textarea type="text" id="description" required rows="5" v-model="value">
+      </textarea>
+      <span class="highlight"></span>
+      <span class="bar description-bar"></span>
+      <label>{{ label }}</label>
+    </div>
+    <p v-else>{{ value }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Textarea',
+  props: {
+    value: {
+      type: String,
+      required: true
+    },
+    label: {
+      type: String,
+      required: true
+    },
+    isEditable: {
+      type: Boolean,
+      default: true
+    }
+  },
+  watch: {
+    value () {
+      this.$emit('input', this.value)
+      console.log(this.value)
+    }
+  },
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style>
+textarea {
+  width: 100%;
+  border: none;
+}
+
+textarea:focus {
+  border: none;
+  outline: none;
+}
+</style>
